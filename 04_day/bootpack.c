c05803edb72e909ffc1730ccd23eccb918781421
Yourtion <yourtion@gmail.com>
2014-09-10 15:54:04 +0800

    另外的写法

--- 04_day/bootpack.c
+++ 04_day/bootpack.c
@@ -5,10 +5,10 @@ void HariMain(void)
 	int i; /* 声明变量i，i是32位整数 */
 	char *p; /* 声明变量p、用于BYTE [...]地址 */
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		p = (char *)i;
-		*p = i & 0x0f;
-		/* 替代write_mem8(i, i & 0x0f); */
+	p = (char *) 0xa0000; /* 地址变量赋值 */
+
+	for (i = 0; i <= 0xffff; i++) {
+		*(p + i) = i & 0x0f;
 	}
 
 	for (;;) {
