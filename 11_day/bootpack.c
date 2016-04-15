28982075605d032fd8f33d0f909eb9b735a0fd60
Yourtion <yourtion@gmail.com>
2016-04-15 11:06:26 +0800

    小实验

--- 11_day/bootpack.c
+++ 11_day/bootpack.c
@@ -52,8 +52,8 @@ void HariMain(void)
 	sheet_slide(sht_mouse, mx, my);
 	sheet_slide(sht_win, 80, 72); 
 	sheet_updown(sht_back,  0);
-	sheet_updown(sht_win, 1);
-	sheet_updown(sht_mouse, 2);
+	sheet_updown(sht_mouse, 1);
+	sheet_updown(sht_win, 2);
 	sprintf(s, "(%3d, %3d)", mx, my);
 	putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 	sprintf(s, "memory %dMB free : %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
