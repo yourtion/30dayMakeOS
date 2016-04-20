36f64a7650721e8f731d7f7ad7e0ee5279439a62
Yourtion <yourtion@gmail.com>
2016-04-20 14:00:08 +0800

    重新加入性能测试代码

--- 13_day/bootpack.c
+++ 13_day/bootpack.c
@@ -13,7 +13,7 @@ void HariMain(void)
 	char s[40];
 	int fifobuf[128];
 	struct TIMER *timer, *timer2, *timer3;
-	int mx, my, i;
+	int mx, my, i, count;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
@@ -74,9 +74,7 @@ void HariMain(void)
 	putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
 
 	for (;;) {
-		sprintf(s, "%010d", timerctl.count);
-		putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
-
+		count++;
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
 			io_sti();
@@ -121,8 +119,11 @@ void HariMain(void)
 				}
 			} else if (i == 10) { /* 10秒定时器 */
 				putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
+				sprintf(s, "%010d", count);
+				putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
 			} else if (i == 3) { /* 3秒定时器 */
 				putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
+				 count = 0; /* 开始测试 */
 			} else if (i == 1) { /* 光标用定时器*/
 				timer_init(timer3, &fifo, 0); /* 下面是设定0 */
 				boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
