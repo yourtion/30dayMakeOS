2b669176d84c83ac0c2b966fc77799593ae025d8
Yourtion <yourtion@gmail.com>
2016-04-26 16:08:19 +0800

    大写字母与小写字母

--- 17_day/bootpack.c
+++ 17_day/bootpack.c
@@ -44,7 +44,7 @@ void HariMain(void)
 		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
 		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
 	};
-	int key_to = 0, key_shift = 0;
+	int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7;
 
 	init_gdtidt();
 	init_pic();
@@ -143,6 +143,11 @@ void HariMain(void)
 					}
 				} else {
 					s[0] = 0;
+				}
+				if ('A' <= s[0] && s[0] <= 'Z') { /*当输入字符为英文字母时*/
+					if (((key_leds & 4) == 0 && key_shift == 0) ||((key_leds & 4) != 0 && key_shift != 0)) {
+						s[0] += 0x20; /*将大写字母转换为小写字母*/
+					}
 				}
 				if (s[0] != 0) { /*一般字符*/
 					if (key_to == 0) { /*发送给任务A */
