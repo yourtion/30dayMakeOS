36da3e87a93bba11785e8b45e1a37454d0341685
Yourtion <yourtion@gmail.com>
2016-05-11 13:16:09 +0800

    窗口切换（1）

--- 24_day/bootpack.c
+++ 24_day/bootpack.c
@@ -233,6 +233,9 @@ void HariMain(void)
 					task_cons->tss.eip = (int) asm_end_app;
 					io_sti();
 				}
+				if (i == 256 + 0x57 && shtctl->top > 2) { /* F11 */
+					sheet_updown(shtctl->sheets[1], shtctl->top - 1);
+				}
 				if (i == 256 + 0xfa) { /*键盘成功接收到数据*/
 					keycmd_wait = -1;
 				}
