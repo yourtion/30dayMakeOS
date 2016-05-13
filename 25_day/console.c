b30660628236fdd2440f8c33f044986ddd81eb72
Yourtion <yourtion@gmail.com>
2016-05-13 10:48:03 +0800

    窗口初始位置

--- 25_day/console.c
+++ 25_day/console.c
@@ -348,8 +348,8 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		sht->flags |= 0x10;
 		sheet_setbuf(sht, (char *) ebx + ds_base, esi, edi, eax);
 		make_window8((char *) ebx + ds_base, esi, edi, (char *) ecx + ds_base, 0);
-		sheet_slide(sht, 100, 50);
-		sheet_updown(sht, 3); /*背景层高度3位于task_a之上*/
+		sheet_slide(sht, (shtctl->xsize - esi) / 2, (shtctl->ysize - edi) / 2);
+		sheet_updown(sht, shtctl->top); /*将窗口图层高度指定为当前鼠标所在图层的高度，鼠标移到上层*/
 		reg[7] = (int) sht; 
 	} else if (edx == 6) {
 		sht = (struct SHEET *) (ebx & 0xfffffffe);
