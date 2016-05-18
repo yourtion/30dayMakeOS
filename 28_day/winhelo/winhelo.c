7b5d69c7c85779d0ba808f321767fb7b82a7db7b
Yourtion <yourtion@gmail.com>
2016-05-18 11:02:51 +0800

    alloca（2）

--- 28_day/winhelo/winhelo.c
+++ 28_day/winhelo/winhelo.c
@@ -1,10 +1,10 @@
 #include "apilib.h"
 
-char buf[150 * 50];
-
 void HariMain(void)
 {
 	int win;
+	char buf[150 * 50];
+
 	win = api_openwin(buf, 150, 50, -1, "hello");
 	for (;;) {
 		if (api_getkey(1) == 0x0a) {
--- 28_day/winhelo2/winhelo2.c
+++ 28_day/winhelo2/winhelo2.c
@@ -1,10 +1,10 @@
 #include "apilib.h"
 
-char buf[150 * 50];
-
 void HariMain(void)
 {
 	int win;
+	char buf[150 * 50];
+	
 	win = api_openwin(buf, 150, 50, -1, "hello");
 	api_boxfilwin(win,  8, 36, 141, 43, 3); /*黄色*/
 	api_putstrwin(win, 28, 28, 0 /*黑色*/, 12, "hello, world");
