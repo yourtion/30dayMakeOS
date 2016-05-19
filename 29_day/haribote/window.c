41a0252634dc00686e2f25570a52b7efc9b64229
Yourtion <yourtion@gmail.com>
2016-05-19 12:27:28 +0800

    修复bug

--- 29_day/haribote/window.c
+++ 29_day/haribote/window.c
@@ -66,9 +66,15 @@ void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
 
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l)
 {
+	struct TASK *task = task_now();
 	boxfill8(sht->buf, sht->bxsize, b, x, y, x + l * 8 - 1, y + 15);
-	putfonts8_asc(sht->buf, sht->bxsize, x, y, c, s);
-	sheet_refresh(sht, x, y, x + l * 8, y + 16);
+	if (task->langmode != 0 && task->langbyte1 != 0) {
+		putfonts8_asc(sht->buf, sht->bxsize, x, y, c, s);
+		sheet_refresh(sht, x - 8, y, x + l * 8, y + 16);
+	} else {
+		putfonts8_asc(sht->buf, sht->bxsize, x, y, c, s);
+		sheet_refresh(sht, x, y, x + l * 8, y + 16);
+	}
 	return;
 }
 
